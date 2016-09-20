6421499a4b1407ba79ad189ed2c2c7b74a45642d
Matt Mazzola <mattm@microsoft.com>
2016-09-20 14:33:58 -0700

    Add stub filter-pane component and setup custom filter scenario

--- src/components/powerbi-filter-pane/component.js
+++ src/components/powerbi-filter-pane/component.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react'
 
 /* global powerbi */
-export class PageNavigation extends Component {
+export class FilterPane extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pages: []
     };
   }
 
   render() {
     return (
-      <div>Page Navigation Placeholder (Not Implemented)</div>
+      <div>Filter Pane Placeholder (Not Implemented)</div>
     )
   }
 }
 
-export default PageNavigation;
+export default FilterPane;
--- src/filters/component.js
+++ src/filters/component.js
@@ -1,9 +1,65 @@
 import React, { Component } from 'react';
+import Embed from '../components/powerbi-component/component';
+import FilterPane from '../components/powerbi-filter-pane/component';
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      embedConfig: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch(`https://powerbiembedapi.azurewebsites.net/api/reports/c52af8ab-0468-4165-92af-dc39858d66ad`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        else {
+          throw new Error(response.json());
+        }
+      })
+      .then(embedConfig => {
+        this.setState({
+          embedConfig
+        });
+
+        return embedConfig;
+      });
+  }
+
+  onEmbedded(embed) {
+    console.log(`Report embedded: `, embed, this);
+  }
+
   render() {
     return (
-      <h2>Custom Filter Pane</h2>
+      <div>
+        <h1>Custom Filter Pane</h1>
+        <p>Filter pane is hidden in the embedded report and recreated by developer to allow custom branding or special preconfigured filters.</p>
+
+        <div className="row">
+          <div className="col-md-9">
+            <div className="row">
+              <div className="col-xs-12">
+                <Embed
+                  options={this.state.embedConfig}
+                  onEmbedded={this.onEmbedded}
+                >
+                </Embed>
+              </div>
+            </div>
+            <div className="row">
+              Pre-defined Filter Buttons (Not Implemented)
+            </div>
+          </div>
+          <div className="col-md-3">
+            <FilterPane></FilterPane>
+          </div>
+        </div>
+      </div>
     );
   }
 }
